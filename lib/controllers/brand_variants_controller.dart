a01c4128f54623743ad490249b22b44fb2450297
nalugala_vc <vanessachebukwa@gmail.com>
2025-01-14 19:20:45 +0300

    Brand variant controller

--- lib/controllers/brand_variants_controller.dart
+++ lib/controllers/brand_variants_controller.dart
@@ -4,8 +4,6 @@ import 'package:get/get_navigation/get_navigation.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:kwajuafrica/core/controller/base_controller.dart';
 import 'package:kwajuafrica/model/brand_variant.dart';
-import 'package:kwajuafrica/model/category.dart';
-import 'package:kwajuafrica/model/featured_product_category.dart';
 import 'package:kwajuafrica/repositories/product_repository.dart';
 
 class BrandVariantsController extends BaseController {
@@ -13,5 +11,31 @@ class BrandVariantsController extends BaseController {
 
   final ProductRepository _productRepository = ProductRepository();
 
-  final brandVariantsLust = RxList<BrandVariant>();
+  final brandVariantsList = RxList<BrandVariant>();
+
+  Future<void> fetchBrandVariants({
+    required String categoryId,
+    required String subCategoryId,
+    required String typeId,
+    required String brandId,
+  }) async {
+    setBusy(true);
+    try {
+      final brandVariants = await _productRepository.fetchBrandVariants(
+          brandId: brandId,
+          categoryId: categoryId,
+          subCategoryId: subCategoryId,
+          typeId: typeId);
+      setBusy(false);
+      if (brandVariants != null && brandVariants.isNotEmpty) {
+        brandVariantsList.assignAll(brandVariants);
+      } else {
+        brandVariantsList.clear();
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'Failed to fetch brand variants');
+    } finally {
+      setBusy(false);
+    }
+  }
 }
